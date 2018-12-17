fb9ffa0969a7dab2bd53a0b0d1628aea32af0b9a
Fazal-haroon <kharoon187@gmail.com>
2018-12-17 07:31:56 +0500

    pizza

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 hungry = input("are you hungry?")
 if hungry == 'yes':
     print("eat samosa ")
+    print("eat pizza also")
+    print("remember junk food not good for health")
 else:
     print("do your homework")
